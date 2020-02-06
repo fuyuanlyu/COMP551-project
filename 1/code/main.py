4d6180d1701cac54b3dba1d123530a3f66b9d3f4
Tommy Lyu <lfy_1997@126.com>
2020-02-05 21:49:35 -0500

    Auto stash before rebase of "origin/master"

--- 1/code/main.py
+++ 1/code/main.py
@@ -1,18 +1,22 @@
 from models import logistic, naivebayes
 from myutility import convertToOneHot
 import numpy as np
+from sklearn.linear_model import LogisticRegression
 
 k_fold = 5
 
 def main():
 	# data = np.load('Datasets/adult_data_cleaned.npy')
 	data = np.load('Datasets/iris_data_cleaned.npy')
+	# data_x = np.load('Datasets/iris_data_features.npy', allow_pickle=True)
+	# data_y = np.load('Datasets/iris_data_target.npy', allow_pickle=True)
 	np.random.shuffle(data)
 	num_samples = data.shape[0]
 	data_x = data[:num_samples,:-1]
-	data_y = data[:num_samples,-1:]
+	data_y = data[:num_samples,-1]
 
 	step = data_x.shape[0] // k_fold
+	# print(data_y)
 	num_of_class = int(np.max(data_y)+1)
 	acc = 0.
 
@@ -29,16 +33,31 @@ def main():
 		LR.fit(train_x, train_y)
 
 		predict_y = LR.predict(test_x)
-		# print(predict_y.shape)
+		# print(predict_y)
 		# print(np.squeeze(test_y).shape)
 		# print(np.sum(predict_y == np.squeeze(test_y)))
-		acc += np.sum(predict_y == np.squeeze(test_y)) / test_y.shape[0]
-		print(np.sum(predict_y == np.squeeze(test_y)) / test_y.shape[0])
+		acc += np.sum(predict_y == test_y) / test_y.shape[0]
 	print(acc/k_fold)
+
+	acc = 0.
+	for i in range(k_fold):
+		test_x, test_y = data_x[i:i+step], data_y[i:i+step]
+		train_x, train_y = np.concatenate((data_x[0:i], data_x[i+step:]), axis=0), np.concatenate((data_y[0:i], data_y[i+step:]), axis=0)
+		
+		LR2 = LogisticRegression(solver='lbfgs', multi_class='multinomial')
+		LR2.fit(train_x, train_y)
+		predict_y = LR2.predict(test_x)
+		# print(predict_y == test_y)
+		acc += np.sum(predict_y == test_y) / test_y.shape[0]
+	print(acc/k_fold)
+
+
+
 
 
 if __name__ == '__main__':
 	main()
+
 
 
 
--- 1/code/models/logistic.py
+++ 1/code/models/logistic.py
@@ -2,12 +2,13 @@ import numpy as np
 # from ..myutility import convertToOneHot
 
 class logistic_regression():
-	def __init__(self, num_of_features, num_of_class, max_iter=1000):
+	def __init__(self, num_of_features, num_of_class, max_iter=100):
 		assert isinstance(num_of_features, int) and num_of_features > 0
 		assert isinstance(max_iter, int) and max_iter > 0
 		assert isinstance(num_of_class, int) and num_of_class > 0
 
 		self.weight = 2 * np.random.random_sample( (num_of_features, num_of_class) ) - 1
+		# self.weight = 2 * np.random.random_sample( (num_of_class, num_of_features) ) - 1
 		self.num_of_features = num_of_features
 		self.max_iter = max_iter
 		self.num_of_class = num_of_class
@@ -22,19 +23,25 @@ class logistic_regression():
 		# print(self.weight.shape)
 		# print(X.shape)
 		for i in range(self.max_iter):
-			predict_y = np.exp( np.matmul(X, self.weight) ) / 1 + np.sum( np.exp( np.matmul(X, self.weight) ) )
-			# predict_y = 1 / (1 + np.exp( - np.dot(X, self.weight.T)))
+			# predict_y = np.exp(-np.matmul(X, self.weight)) / (1 + np.sum(np.exp(-np.matmul(X, self.weight))))
+			predict_y = 1 / (1 + np.exp( - np.matmul(X, self.weight)))
 			gradient = np.dot(X.T, predict_y - y)
+			# print(gradient.shape)
 			# if gradient.ndim == 2:
 			# 	gradient = np.sum(gradient, axis=1)
 			self.weight -= gradient * lr
+		# print(self.weight.shape)
 		return self
 
 	def predict(self, X):
-		predict_y = np.exp( np.matmul(X, self.weight) ) / 1 + np.sum( np.exp( np.matmul(X, self.weight) ) )
+		# predict_y = np.exp(-np.matmul(X, self.weight)) / (1 + np.sum(np.exp(-np.matmul(X, self.weight))))
+		predict_y = 1 / (1 + np.exp( - np.matmul(X, self.weight)))
 		# y = np.where(predict_y < 0.5, 0, 1) 
-		y = np.max(predict_y, axis=1)
-		# y = predict_y
+		# max_y = np.max(predict_y, axis=1)
+		# y = np.where(predict_y == max_y)
+		# print(predict_y)
+		# print(y)
+		y = np.argmax(predict_y, axis=1)
 		return y
 
 
