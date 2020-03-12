ed43751f846a1bd35e12e0b22531bc12628d3f4f
SHITIANYU <38762389+SHITIANYU-hue@users.noreply.github.com>
2020-03-12 11:34:18 -0400

    add lstm experiments

---
+++ 2/code/lstm-exp-architecture/bilstm-imdb.py
@@ -0,0 +1,68 @@
+from __future__ import print_function
+from sklearn.feature_extraction.text import CountVectorizer, TfidfTransformer
+from keras.preprocessing import sequence
+from keras.models import Sequential
+from keras.layers import Bidirectional
+from keras.layers import Dense, Embedding
+from keras.layers import LSTM
+from keras.datasets import imdb
+from keras.layers.convolutional import  Conv1D
+from keras.layers.convolutional import MaxPooling1D
+import time
+def LSTM_IMDB():
+    max_features = 20000##5000
+    # cut texts after this number of words (among top max_features most common words)
+    maxlen = 500
+    batch_size = 64
+
+    print('Loading data...')
+    # (x_train, y_train), (x_test, y_test) = imdb.load_data(num_words=max_features)
+    import numpy as np
+    # save np.load
+    np_load_old = np.load
+    # modify the default parameters of np.load
+    np.load = lambda *a,**k: np_load_old(*a, allow_pickle=True, **k)
+    # call load_data with allow_pickle implicitly set to true
+    (x_train, y_train), (x_test, y_test) = imdb.load_data(num_words=10000)
+    # restore np.load for future normal usage
+    np.load = np_load_old
+    print(len(x_train), 'train sequences')
+    print(len(x_test), 'test sequences')
+
+    print('Pad sequences (samples x time)')
+    x_train = sequence.pad_sequences(x_train, maxlen=maxlen)
+    x_test = sequence.pad_sequences(x_test, maxlen=maxlen)
+    print('x_train shape:', x_train.shape)
+    print('x_test shape:', x_test.shape)
+
+    print('Build model...')
+    model = Sequential()
+    model.add(Embedding(max_features, 128,input_length=maxlen))
+    model.add(Conv1D(filters=32,kernel_size=3,padding='same',activation='relu'))
+    model.add(Conv1D(filters=16,kernel_size=3,padding='same',activation='relu'))
+    model.add(MaxPooling1D(pool_size=2))
+
+    model.add(Bidirectional(LSTM(128,dropout=0.1)))
+    # model.add(LSTM(128))
+    model.add(Dense(1, activation='sigmoid')) #sigmoid=0.82
+
+    # try using different optimizers and different optimizer configs
+    model.compile(loss='binary_crossentropy',
+                  optimizer='adam',
+                  metrics=['accuracy'])
+
+    print('Train...')
+    start=time.clock()
+    model.fit(x_train, y_train,
+              batch_size=batch_size,
+              epochs=3,
+              validation_data=(x_test, y_test))
+    end=time.clock()
+    print('total train time:', end-start)
+    score, acc = model.evaluate(x_test, y_test,
+                                batch_size=batch_size)
+    print('Test score:', score)
+    print('Test accuracy:', acc)
+
+if __name__ == '__main__':
+    LSTM_IMDB()
---
+++ 2/code/lstm-exp-architecture/lstm-20news.py
@@ -0,0 +1,120 @@
+from sklearn.datasets import fetch_20newsgroups
+# Load tools we need for preprocessing
+import urllib3
+from keras.preprocessing.text import Tokenizer
+from keras.preprocessing.sequence import pad_sequences
+import numpy as np
+from keras.utils import to_categorical
+import os
+from keras.models import Sequential
+from keras.layers import LSTM, Dense, Activation, Embedding
+from keras.layers.convolutional import  Conv1D
+from keras.layers.convolutional import MaxPooling1D
+import time
+class LSTM_20news():
+    def __init__(self,vocab_size = 20000,max_length = 1000,embedding_dim = 100):
+        self.vocab_size=vocab_size
+        self.max_length=max_length
+        self.embedding_dim=embedding_dim
+
+    # def main_train(self):
+        ##process trainning dataset
+        twenty_train = fetch_20newsgroups(subset='train', remove=['headers', 'footers', 'quote'], shuffle=True)
+        texts = twenty_train.data # Extract text
+        target = twenty_train.target # Extract target
+        tokenizer_train = Tokenizer(num_words=self.vocab_size) # Setup tokenizer
+        tokenizer_train.fit_on_texts(texts)
+        sequences = tokenizer_train.texts_to_sequences(texts) # Generate sequences
+        data = pad_sequences(sequences, maxlen=self.max_length)
+        ##Turning labels into One-Hot encodings¶
+        labels = to_categorical(np.asarray(target))
+
+        word_index = tokenizer_train.word_index
+        # Create inverse index mapping numbers to words
+        inv_index = {v: k for k, v in tokenizer_train.word_index.items()}
+
+
+        ##Loading GloVe embeddings
+        embeddings_index = {} # We create a dictionary of word -> embedding
+        if os.path.isfile('glove.6B.100d.txt'):
+            f = open('glove.6B.100d.txt') # Open file
+        else:
+            print('download it from website')
+            response = urllib3.urlopen('https://www.kaggle.com/terenceliu4444/glove6b100dtxt/download')
+            data = response.read()
+            filename = "glove.6B.100d.txt"
+            file_ = open(filename, 'w')
+            file_.write(data)
+            file_.close()
+        # In the dataset, each line represents a new word embedding
+        # The line starts with the word and the embedding values follow
+        for line in f:
+            values = line.split()
+            word = values[0] # The first value is the word, the rest are the values of the embedding
+            embedding = np.asarray(values[1:], dtype='float32') # Load embedding
+            embeddings_index[word] = embedding # Add embedding to our embedding dictionary
+        f.close()
+
+        # Create a matrix of all embeddings
+        all_embs = np.stack(embeddings_index.values())
+        emb_mean = all_embs.mean() # Calculate mean
+        emb_std = all_embs.std() # Calculate standard deviation
+        word_index = tokenizer_train.word_index
+        nb_words = min(self.vocab_size, len(word_index)) # How many words are there actually
+
+        # Create a random matrix with the same mean and std as the embeddings
+        embedding_matrix = np.random.normal(emb_mean, emb_std, (nb_words, self.embedding_dim))
+
+        # The vectors need to be in the same position as their index.
+        # Meaning a word with token 1 needs to be in the second row (rows start with zero) and so on
+
+        # Loop over all words in the word index
+        for word, i in word_index.items():
+            # If we are above the amount of words we want to use we do nothing
+            if i >= self.vocab_size:
+                continue
+            # Get the embedding vector for the word
+            embedding_vector = embeddings_index.get(word)
+            # If there is an embedding vector, put it in the embedding matrix
+            if embedding_vector is not None:
+                embedding_matrix[i] = embedding_vector
+
+
+        model = Sequential()
+        model.add(Embedding(self.vocab_size,
+                            self.embedding_dim,
+                            input_length=self.max_length,
+                            weights = [embedding_matrix],
+                            trainable = False))
+        # model.add(Conv1D(filters=32,kernel_size=3,padding='same',activation='relu'))
+        # model.add(MaxPooling1D(pool_size=2))
+        model.add(LSTM(128))
+
+        model.add(Dense(20,input_dim=20,activation='sigmoid'))
+        # model.add(Activation('softmax'))
+        model.summary()
+        model.compile(optimizer='adam',
+                      loss='binary_crossentropy',
+                      metrics=['acc'])
+        start=time.clock()
+        model.fit(data,labels,validation_split=0.3,epochs=40)
+        end = time.clock()
+        print('total train time:', end - start)
+    # def main_test(self):
+        ##process testing dataset
+        twenty_test = fetch_20newsgroups(subset='test', remove=['headers', 'footers', 'quote'], shuffle=True)
+        texts_test= twenty_test.data # Extract text
+        targets_test=twenty_test.target# Extract target
+        tokenizer_test = Tokenizer(num_words=self.vocab_size) # Setup tokenizer
+        tokenizer_test.fit_on_texts(texts)
+        sequences_test = tokenizer_test.texts_to_sequences(texts_test) # Generate sequences for texting
+        data_test=pad_sequences(sequences_test, maxlen=self.max_length)
+        ##Turning labels into One-Hot encodings¶
+        labels_test=to_categorical(np.asarray(targets_test))
+        lstm_predicted= model.predict(data_test)
+        print('lstm', lstm_predicted)
+        print('label',labels_test)
+        print('accuracy',np.mean(np.argmax(lstm_predicted,axis=1) == np.argmax(labels_test,axis=1)))
+
+if __name__ == '__main__':
+    LSTM_20news()
